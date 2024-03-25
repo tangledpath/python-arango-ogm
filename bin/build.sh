6f6cee35bf68188dedbdbdee22b48c63fc9b6858
Steven Miers <steven.miers@gmail.com>
2024-03-25 02:45:44 -0500

    Better docs?

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,5 @@
 source ${BASH_SOURCE%/*}/clean.sh
 # Run in browser:
 #poetry run pdoc python_arango_ogm/* --logo https://github.com/tangledpath/python-arango-ogm/blob/master/pao.png --favicon https://github.com/tangledpath/python-arango-ogm/blob/master/pao_sm.png
-poetry run pdoc python_arango_ogm/* -o ./docs --logo https://github.com/tangledpath/python-arango-ogm/blob/master/pao.png --favicon https://github.com/tangledpath/python-arango-ogm/blob/master/pao_sm.png
+poetry run pdoc python_arango_ogm/* -o ./docs --logo https://github.com/tangledpath/python-arango-ogm/blob/master/pao.png?raw=true --favicon https://github.com/tangledpath/python-arango-ogm/blob/master/pao_sm.png?raw=true
 poetry build
